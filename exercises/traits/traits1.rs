ceded3093820ee5bb1955d66bb1ed481a27a5b8d
Amadeuus <1095980257@qq.com>
2025-04-03 15:21:54 +0800

    to_Amaduus

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -15,6 +15,11 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for type `String`.
+    fn appand_bar(self) -> Self {
+        let mut s = self;
+        s.push_str("Bar");
+        s
+    }
 }
 
 fn main() {
